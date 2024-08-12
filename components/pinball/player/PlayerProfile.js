25b67839f5a37ef1f4c66963aeb0d701cf16a075
Eric Brousseau <emb417@gmail.com>
2024-08-11 20:49:47 -0700

    added annual player rivals

--- components/pinball/player/PlayerProfile.js
+++ components/pinball/player/PlayerProfile.js
@@ -1,15 +1,15 @@
 import _ from "lodash";
+import { redirect } from "next/navigation";
 import PlayerSummaryData from "@/lib/pinball/PlayerStats";
 import PlayerBio from "@/components/pinball/player/PlayerBio";
+import PlayerRivals from "@/components/pinball/player/PlayerRivals";
 import PlayerSeasonSummary from "@/components/pinball/player/PlayerSeasonSummary";
-import SeasonLeaderboard from "@/components/pinball/SeasonLeaderboard";
+import PlayerSeasonRivals from "@/components/pinball/player/PlayerSeasonRivals";
 import PlayerSeasonInsights from "@/components/pinball/player/PlayerSeasonInsights";
 import PlayerHistory from "@/components/pinball/player/PlayerHistory";
 import PlayerAnnualInsights from "@/components/pinball/player/PlayerAnnualInsights";
 import PlayerSeasonChart from "@/components/pinball/player/PlayerSeasonChart";
 import PlayerPositionChart from "@/components/pinball/player/PlayerPositionChart";
-import { redirect } from "next/navigation";
-import PlayerSeasonRivals from "./PlayerSeasonRivals";
 
 async function getPlayerSummaryData(username) {
   try {
@@ -27,6 +27,7 @@ async function getPlayerSummaryData(username) {
 
 export default async function PlayerProfile({ username }) {
   const {
+    playerRivals,
     user,
     userPositionData,
     userSeasonSummary,
@@ -42,9 +43,18 @@ export default async function PlayerProfile({ username }) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-12 2xl:grid-cols-12 w-full gap-4">
       <div className="flex flex-col lg:col-span-5 xl:col-span-7 2xl:col-span-6 gap-4">
-        <PlayerBio user={user} />
-        <PlayerSeasonSummary user={user} userSeasonSummary={userSeasonSummary} />
-        <PlayerSeasonRivals userSeasonRivalsByWinPercentage={userSeasonRivalsByWinPercentage} userSeasonRivalsByPoints={userSeasonRivalsByPoints} />
+        <div className="flex grid grid-cols-1 md:grid-cols-2 items-center gap-4">
+          <PlayerBio user={user} />
+          <PlayerRivals playerRivals={playerRivals} />
+        </div>
+        <PlayerSeasonSummary
+          user={user}
+          userSeasonSummary={userSeasonSummary}
+        />
+        <PlayerSeasonRivals
+          userSeasonRivalsByWinPercentage={userSeasonRivalsByWinPercentage}
+          userSeasonRivalsByPoints={userSeasonRivalsByPoints}
+        />
         <PlayerSeasonInsights user={user} />
         <PlayerHistory weeksData={userPositionData} />
         <PlayerAnnualInsights user={user} />
---
+++ components/pinball/player/PlayerRivals.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { CgInfo, CgChevronUpO, CgChevronDownO } from "react-icons/cg";
+import { Tooltip } from "antd";
+import Link from "next/link";
+
+export default function PlayerRivals({ playerRivals }) {
+  return (
+    <div className="flex flex-col gap-2 w-full sm:w-2/3 md:w-full">
+      {playerRivals.map(
+        (rival, index) =>
+          rival.username && (
+            <Link
+              key={index}
+              href={`/pinball/player/${rival.username}`}
+              className="flex flex-row gap-2 text-white text-sm border-2 border-teal-950 rounded-full bg-slate-900"
+            >
+              <div className="flex items-center gap-1 pr-2 col-span-2">
+                <Image
+                  src={rival.userAvatarUrl}
+                  width={32}
+                  height={32}
+                  alt={rival.username}
+                  className="rounded-full"
+                />
+                <span className="truncate">{rival.username}</span>
+              </div>
+              <div className="flex justify-end items-center ml-auto">
+                <Tooltip title="Rolling Average Position" className="flex">
+                  <span className="text-lg text-teal-300">
+                    P{rival.rollingAveragePosition}
+                  </span>
+                  <CgInfo className="text-white text-xs" />
+                </Tooltip>
+              </div>
+              <div className="flex gap-2 items-center justify-end">
+                <Tooltip title="Win Percentage">
+                  <span className="text-lg text-white">
+                    {rival.winPercentage}%
+                  </span>
+                </Tooltip>
+                <span className="w-8 h-8">
+                  {index === 0 && (
+                    <CgChevronUpO className="text-green-500 w-8 h-8" />
+                  )}
+                  {index === 1 && (
+                    <CgChevronDownO className="text-red-500 w-8 h-8" />
+                  )}
+                </span>
+              </div>
+            </Link>
+          )
+      )}
+    </div>
+  );
+}
