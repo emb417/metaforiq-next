b028a3fa47dc1012126f8fb5d55bd3040246405b
Eric Brousseau <emb417@gmail.com>
2024-08-13 19:12:24 -0700

    more responsive spacing for leaderboards

--- components/pinball/SeasonDashboard.js
+++ components/pinball/SeasonDashboard.js
@@ -27,10 +27,10 @@ export default async function SeasonDashboard() {
   const { weeksData } = props;
   return (
     <div className="grid grid-cols-12 mb-14 gap-4 w-full">
-      <div className="col-span-12 sm:col-span-5 md:col-span-4 xl:col-span-3">
+      <div className="col-span-12 sm:col-span-8 md:col-span-6 lg:col-span-4 xl:col-span-3">
         <SeasonLeaderboard weeksData={weeksData} />
       </div>
-      <div className="mt-10 col-span-12 sm:col-span-7 md:col-span-8 xl:col-span-9">
+      <div className="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-8 xl:col-span-9">
         <SeasonChart weeksData={weeksData} />
       </div>
     </div>
--- components/pinball/WeeklyDashboard.js
+++ components/pinball/WeeklyDashboard.js
@@ -24,10 +24,10 @@ export default async function WeeklyDashboard() {
   const { weeksData } = props;
   return (
     <div className="grid grid-cols-12 mb-14 gap-4 w-full">
-      <div className="col-span-12 sm:col-span-5 md:col-span-4 lg:col-span-3 xl:col-span-3">
+      <div className="col-span-12 sm:col-span-8 md:col-span-6 lg:col-span-4 xl:col-span-3">
         <WeeklyLeaderboard weekData={weeksData[0]} />
       </div>
-      <div className="col-span-12 sm:col-span-7 md:col-span-8 lg:col-span-9 xl:col-span-9">
+      <div className="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-8 xl:col-span-9">
         <WeeklyChart
           weeksData={weeksData}
           topPlayers={_.take(_.map(weeksData[0].scores, "username"), 3)}
