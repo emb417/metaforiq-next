5965848de594163b2cc03f6a0fa0da7e51369521
Eric Brousseau <emb417@gmail.com>
2024-08-15 17:20:38 -0700

    line height to tighten up table name

--- components/pinball/player/PlayerSummaryItem.js
+++ components/pinball/player/PlayerSummaryItem.js
@@ -2,7 +2,7 @@ export default function PlayerSummaryItem({ children, title }) {
   return (
     <div className="flex flex-col items-center rounded-lg px-2 pb-1 w-full">
       <div className="text-sm text-white text-center pb-1">{title}</div>
-      <div className="text-teal-300 text-center w-full">{children}</div>
+      <div className="text-teal-300 text-center w-full leading-3">{children}</div>
     </div>
   );
 }
