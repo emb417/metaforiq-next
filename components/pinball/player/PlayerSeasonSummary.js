9c6eb377fa261dcf304add45583bead2132cf5fe
Eric Brousseau <emb417@gmail.com>
2024-08-15 17:01:54 -0700

    spacing tweak for season summary in portrait

--- components/pinball/player/PlayerSeasonSummary.js
+++ components/pinball/player/PlayerSeasonSummary.js
@@ -1,8 +1,8 @@
 export default function PlayerSummary({ user, userSeasonSummary }) {
   return (
-    <div className="flex flex-row gap-4 border-2 rounded-xl px-4 border-teal-950 bg-slate-950 items-center w-full md:w-[max-content]">
+    <div className="flex flex-row gap-3 border-2 rounded-xl px-2 border-teal-950 bg-slate-950 items-center w-full sm:w-[max-content]">
       <div className="text-sm text-white">Season 5</div>
-      <div className="text-xl text-teal-300">
+      <div className="text-xl text-teal-300 ml-auto">
       P{userSeasonSummary.position}{" "}
           <span className="text-sm">
             of {userSeasonSummary.numberOfPlayers}
