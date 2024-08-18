18de39cc170b28ec75a280458d5e9886eff4cf06
Eric Brousseau <emb417@gmail.com>
2024-08-17 17:49:04 -0700

    linking best and worst performances to weekly history

--- components/pinball/player/PlayerAnnualInsights.js
+++ components/pinball/player/PlayerAnnualInsights.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PlayerPane from "@/components/pinball/player/PlayerPane";
 import PlayerSummaryItem from "@/components/pinball/player/PlayerSummaryItem";
 
@@ -9,19 +10,48 @@ export default function PlayerSummary({ user }) {
         className="flex flex-col md:flex-row items-center md:items-start"
       >
         <PlayerSummaryItem title="Best Game to Average">
-          <span className="text-sm">
-            P{user.annualBestPerformancePosition} of {user.annualBestPerformancePlayers}
-            <span className="text-white"> | </span>
-            {user.annualBestPerformanceTable}
-          </span>
+          {user.annualBestPerformanceWeek !== 0 && (
+            <Link
+              href={`/pinball/history?week=${user.annualBestPerformanceWeek}`}
+            >
+              <span className="text-sm">
+                P{user.annualBestPerformancePosition} of{" "}
+                {user.annualBestPerformancePlayers}
+                <span className="text-white"> | </span>
+                {user.annualBestPerformanceTable}
+              </span>
+            </Link>
+          )}
+          {user.annualBestPerformanceWeek === 0 && (
+            <span className="text-sm">
+              P{user.annualBestPerformancePosition} of{" "}
+              {user.annualBestPerformancePlayers}
+              <span className="text-white"> | </span>
+              {user.annualBestPerformanceTable}
+            </span>
+          )}
         </PlayerSummaryItem>
         <PlayerSummaryItem title="Worst Game to Average">
-          <span className="text-sm">
-            P{user.annualWorstPerformancePosition} of{" "}
-            {user.annualWorstPerformancePlayers}
-            <span className="text-white"> | </span>
-            {user.annualWorstPerformanceTable}
-          </span>
+          {user.annualWorstPerformanceWeek !== 0 && (
+            <Link
+              href={`/pinball/history?week=${user.annualWorstPerformanceWeek}`}
+            >
+              <span className="text-sm">
+                P{user.annualWorstPerformancePosition} of{" "}
+                {user.annualWorstPerformancePlayers}
+                <span className="text-white"> | </span>
+                {user.annualWorstPerformanceTable}
+              </span>
+            </Link>
+          )}
+          {user.annualWorstPerformanceWeek === 0 && (
+            <span className="text-sm">
+              P{user.annualWorstPerformancePosition} of{" "}
+              {user.annualWorstPerformancePlayers}
+              <span className="text-white"> | </span>
+              {user.annualWorstPerformanceTable}
+            </span>
+          )}
         </PlayerSummaryItem>
       </PlayerPane>
     </div>
