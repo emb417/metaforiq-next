b90938a89b56424074b18f0fbca44c6ffbf9fc9c
Eric Brousseau <emb417@gmail.com>
2024-08-19 16:33:57 -0700

    removing position remnants and redirecting to weekly

--- app/pinball/position/page.js
+++ app/pinball/position/page.js
@@ -1,47 +1,6 @@
-import { Suspense } from "react";
-import { GiPinballFlipper, GiPositionMarker } from "react-icons/gi";
-import { MdLeaderboard } from "react-icons/md";
-import PageTitle from "@/components/nav/PageTitle";
-import SubNav from "@/components/nav/SubNav";
-import PositionDashboard from "@/components/pinball/PositionDashboard";
-
-export const metadata = {
-  title: "Position Trends",
-};
-
-const navItems = [
-  {
-    id: 1,
-    icon: <GiPinballFlipper />,
-    href: "/pinball",
-    text: "Pinball Leaderboards",
-  },
-  {
-    id: 2,
-    icon: <MdLeaderboard />,
-    href: "/pinball/season",
-    text: "Season Leaderboard",
-  },
-];
+import { redirect } from "next/navigation";
 
 export default function PositionPage() {
-  return (
-    <div className="flex flex-wrap w-full px-4">
-      <div className="flex flex-wrap w-full mb-4 items-center">
-        <PageTitle>
-          <GiPositionMarker /> {metadata.title}
-        </PageTitle>
-        <SubNav navItems={navItems} />
-      </div>
-      <Suspense
-        fallback={
-          <div className="w-full text-2xl text-white text-center flex justify-center items-center">
-            Loading Position Trends...
-          </div>
-        }
-      >
-        <PositionDashboard />
-      </Suspense>
-    </div>
-  );
+  redirect("/pinball/weekly");
+  return;
 }
