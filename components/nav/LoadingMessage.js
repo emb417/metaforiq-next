8245b40ff4ee46dde2900e4264c4a133147a206b
Eric Brousseau <emb417@gmail.com>
2024-08-27 18:22:54 -0700

    loading size halfed for better ratio to text

--- components/nav/LoadingMessage.js
+++ components/nav/LoadingMessage.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LoadingMessage() {
     return <div className="h-screen w-screen flex flex-col items-center justify-start pt-4 gap-4">
-            <Image src="/icon.png" width={256} height={256} alt="Loading..." className="animate-pulse" />
+            <Image src="/icon.png" width={128} height={128} alt="Loading..." className="animate-pulse" />
             <div className="text-4xl text-white animate-pulse">Loading...</div>
         </div>
   }
