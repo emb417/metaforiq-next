a675007fc88401453288498ec09f84be0c5a4e65
Eric Brousseau <emb417@gmail.com>
2025-01-17 14:24:13 -0800

    added site version to loading message

--- components/nav/LoadingMessage.js
+++ components/nav/LoadingMessage.js
@@ -1,8 +1,21 @@
 import Image from "next/image";
 
-export default function LoadingMessage() {
-    return <div className="h-screen w-screen flex flex-col items-center justify-start pt-4 gap-4">
-            <Image src="/icon.png" width={128} height={128} alt="Loading..." className="animate-pulse" />
-            <div className="text-4xl text-white animate-pulse">Loading...</div>
-        </div>
-  }
+export default function LoadingMessage({ message = "Loading..." }) {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-start pt-4 gap-4">
+      <Image
+        src="/icon.png"
+        width={128}
+        height={128}
+        alt={`${message}`}
+        className="animate-pulse"
+      />
+      <div className="text-center text-4xl text-white animate-pulse">
+        {message}
+      </div>
+      <div className="text-center text-xs text-teal-300">
+        metaforiq-next v{process.env.npm_package_version}
+      </div>
+    </div>
+  );
+}
