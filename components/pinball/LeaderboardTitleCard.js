a5f52ffa33502fb86d56f5235ff8a45f2d1b7444
Eric Brousseau <emb417@gmail.com>
2025-04-21 07:52:12 -0700

    updated styling of empty b2sFile image

--- components/pinball/LeaderboardTitleCard.js
+++ components/pinball/LeaderboardTitleCard.js
@@ -7,17 +7,17 @@ export default function LeaderboardTitleCard({
   children,
 }) {
   return (
-    <div className={`relative mb-2 ${!highlighted || "text-orange-300"} text-center`}>
+    <div className={`relative mb-2 ${!highlighted || "text-teal-300"} text-center`}>
       <Image
         src={
           imageUrl ??
-          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEkAAAA3CAYAAABAW2dtAAAAYElEQVR42u3QIQEAMAgAsNPlCPoHhAY43BZhUfn7sQpJkiRJkiRJkiQkSZIkSZIkSUiSJEmSJEmSJGmQJEmSJEmSJCFJkiRJkiRJQpIkSZIkSZIkCUmSJEmSJEkSkiQdGEMLT3+vKf5nAAAAAElFTkSuQmCC"
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkF9f+DwAB4AFSb/p0HAAAAABJRU5ErkJggg=="
         }
         width={imageUrl ? 292 : 292}
         height={imageUrl ? 292 : 220}
         alt={table}
         priority={true}
-        className="border-2 border-orange-950 rounded-xl opacity-50"
+        className="border-2 border-teal-950 rounded-xl opacity-50"
       />
       <div className="absolute bottom-0 rounded-xl m-1 w-[284px]">
         {children}
