3fb69ac9c20aa545478c71eb0dec7ad5017b6523
Eric Brousseau <emb417@gmail.com>
2026-02-14 21:52:35 -0800

    updated sitemap to include all pages

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,8 +3,20 @@ export default function sitemap() {
     {
       url: "https://www.metaforiq.com",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: "https://www.metaforiq.com/impact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.metaforiq.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
