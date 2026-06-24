fc52d683f2cab51014bcaa332bd486f181e041df
Eric Brousseau <emb417@gmail.com>
2026-06-24 09:20:10 -0700

    fix for smooth scrolling

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>
         <NavBar />
         {children}
